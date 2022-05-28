431f08102ea2b15c193d4643d9922e4e6ae40ea5
El3omda <el3om0a@gmail.com>
2022-05-28 18:12:23 +0200

    Create Nav And Footer

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,68 @@
+<footer class="bg-primary text-white text-center text-lg-start mt-5">
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Barmajli</h5>
+
+                <p>
+                    Its A Site Helps You To Find Best Programmer To Help You Finish Your Project And Gives Your Idea Soul To Be A Live
+                </p>
+
+                <div class="mt-2">
+                    <h5 class="mt-4 mb-3">Payment Methods</h5>
+                    <i class="fa-brands fa-cc-visa fa-2xl"></i>
+                    <i class="fa-brands fa-cc-mastercard fa-2xl"></i>
+                    <i class="fa-brands fa-cc-paypal fa-2xl"></i>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Main Links</h5>
+
+                <ul class="list-unstyled mb-0">
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">Home</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">Programmers</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">Join Us</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase mb-0">&nbsp;</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">Privacy Police</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">Rights and Duties</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">Usage Policy</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white text-decoration-none">FAQ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © {{ date('Y') }} Copyright Emad Othman
+    </div>
+</footer>
---
+++ resources/views/inc/nav.blade.php
@@ -0,0 +1,93 @@
+<style>
+    .dropdown-menu {
+        left: -100px;
+        top: 50px;
+    }
+
+    .navbar-dark .nav-item .nav-link {
+        color: #fff;
+    }
+
+    .navbar-dark .nav-item .nav-link:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+        transition: all 0.3s ease;
+        border-radius: 0.25rem;
+        color: #fff;
+    }
+
+    .fa-li {
+        position: relative;
+        left: 0;
+    }
+
+</style>
+
+<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold" href="/">
+            Barmajli
+        </a>
+
+        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/projects">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/programmers">Programmers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact">Contact Us</a>
+                </li>
+            </ul>
+
+            <form class="d-flex input-group w-auto">
+                <input type="search" class="form-control shadow-none" placeholder="Word To Search . . ." aria-label="Search" />
+                <button class="btn btn-outline-primary shadow-none" type="button" data-mdb-ripple-color="dark"
+                    style="padding: .45rem 1.5rem .35rem;">
+                    Search
+                </button>
+            </form>
+
+            <ul class="navbar-nav mb-2 mb-lg-0">
+
+                <li class="nav-item dropdown">
+                    <a class="btn dropdown noti-rel shadow-none" role="button" type="button" href="#" id="notidrop" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fa fa-solid fa-bell fa-lg"></i>
+                    </a>
+                    <div class="dropdown-menu pull-left" aria-labelledby="notidrop">
+
+                        <a class="dropdown-item">Notification</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown float-end">
+                    <a class="btn dropdown noti-rel shadow-none" role="button" type="button" href="#" id="userdrop" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-2379004.jpg&fm=jpg"
+                            class="rounded-circle" height="30" width="30" />
+                    </a>
+                    <div class="dropdown-menu pull-right" aria-labelledby="userdrop">
+                        <a class="dropdown-item" href="/profile">Profile</a>
+                        <a class="dropdown-item" href="/messages">Messages</a>
+                        <a class="dropdown-item" href="/help">Help</a>
+                        <a class="dropdown-item" href="/balance">Balance</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="/logout">Logout</a>
+                    </div>
+                </li>
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
